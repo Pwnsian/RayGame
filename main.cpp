bb964f5b29f895c2a8da63654dedec62da6f9bf8
Pwnsian <omgtehpwns@yahoo.com>
2019-02-24 11:14:43 -0500

    Add tile colors

--- main.cpp
+++ main.cpp
@@ -3,9 +3,18 @@
 #include <cmath>
 #include <iostream>
 #include <sstream>
+#include <unordered_map>
 
 typedef std::unique_ptr<SDL_Renderer, decltype(SDL_DestroyRenderer)> SDLRendererPtr;
 typedef std::array<std::array<char,16>,16> GameMap;
+
+std::unordered_map<char, SDL_Color> mapColors = {
+    {'0', SDL_Color {162, 151, 163, 255} },
+    {'1', SDL_Color {190, 83, 83, 255} },
+    {'2', SDL_Color {249, 252, 241, 255} },
+    {'3', SDL_Color {82, 107, 121, 255} },    
+    {' ', SDL_Color {0, 0, 0, 0} },  
+};
 
 //SDLRendererPtr MakeSDLRenderer(SDL_Surface* surface)
 //{
@@ -77,8 +86,9 @@ void draw_map(const GameMap& map, SDL_Surface* surface)
         char map_i_j = map[row][col];
         if(map_i_j == ' ')
             continue;
-        SDL_Rect wall { to_pixels_x(map, surface, col), to_pixels_y(map, surface, row), block_size_w, block_size_h };            
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_Rect wall { to_pixels_x(map, surface, col), to_pixels_y(map, surface, row), block_size_w, block_size_h };
+        SDL_Color& wallColor = mapColors.at(map_i_j);
+        SDL_SetRenderDrawColor(renderer, wallColor.r, wallColor.g, wallColor.b, wallColor.a);
         SDL_RenderFillRect(renderer, &wall);
     }
 }
@@ -88,20 +98,27 @@ float to_radians(float degrees)
     return (M_PI / 180.f) * degrees;
 }
 
-float trace_ray(float start_x, float start_y, float ang, const GameMap& map, SDL_Surface* surface)
+// Distance of ray and map tile hit.
+std::pair<float, char> trace_ray(float start_x, float start_y, float ang, const GameMap& map, SDL_Surface* surface)
 {
     float c = 0;
+    char map_tile_hit = ' ';
+
     for (; c < 20; c += .05) 
     {
         float x = start_x + c * cos(ang);
         float y = start_y + c * sin(ang);
-        if (map[(int)y][(int)x] != ' ')
+        char map_x_y = map[(int)y][(int)x];
+        if (map_x_y != ' ')
+        {
+            map_tile_hit = map_x_y;
             break;
+        }
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderDrawPoint(renderer, to_pixels_x(map, surface, x), to_pixels_y(map, surface, y));
     }
 
-    return c;
+    return std::make_pair(c, map_tile_hit);
 }
 
 float trace_player_fov_rays(const player& p, const GameMap& map, SDL_Surface* surface)
@@ -114,11 +131,17 @@ float trace_player_fov_rays(const player& p, const GameMap& map, SDL_Surface* su
     int ray_number = 0;
     for(float current_ang = start_ang; current_ang < end_ang; current_ang += (p.fov / 512.0), ++ray_number)
     {
-        float ray_dist = trace_ray(p.x, p.y, current_ang, map, surface);
+        auto cast_result = trace_ray(p.x, p.y, current_ang, map, surface);
+        float ray_dist = cast_result.first;
+        char tile_hit = cast_result.second;
+
         int column_x = map_render_size_w + ray_number;
         int column_w = 1;
         int column_h = (ray_dist > 0) ? full_image_size_h / ray_dist : ray_dist;
         int column_y = full_image_size_h/2 - column_h/2;
+
+        SDL_Color tile_color = mapColors.at(tile_hit);
+        SDL_SetRenderDrawColor(renderer, tile_color.r, tile_color.g, tile_color.b, tile_color.a);
         SDL_RenderDrawLine(renderer, column_x, column_y, column_x, column_y + column_h);
     }
 }
